92b81afd17e5b013d07100eb9f449950280a4c19
CK Tan <tanck105@hotmail.com>
2018-01-23 16:41:41 +0800

    add JSONParse, some utilities class

---
+++ app/src/main/java/com/example/yello/inventory_mvc/utility/Key.java
@@ -0,0 +1,44 @@
+package com.example.yello.inventory_mvc.utility;
+
+/**
+ * Created by CK Tan on 1/23/2018.
+ */
+
+public final class Key
+{
+    // TO BE SAME AS THE PROPERTIES NAME IN WCF
+    
+    // FOR STATIONERY
+    public static final String STATIONERY_1_ITEM_CODE = "ItemCode";
+    public static final String STATIONERY_2_DESCRIPTION = "Description";
+    public static final String STATIONERY_3_UOM = "UOM";
+    public static final String STATIONERY_4_CATEGORY = "Category";
+    public static final String STATIONERY_5_LOCATION = "Location";
+    
+    
+    // FOR REQUISITION RECORD
+    public static final String REQUISITION_RECORD_1_REQUISITION_NO = "RequisitionNo";
+    public static final String REQUISITION_RECORD_2_DEPT_CODE = "DeptCode";
+    public static final String REQUISITION_RECORD_3_DEPT_NAME = "DeptName";
+    public static final String REQUISITION_RECORD_4_REQUESTER_ID = "RequesterID";
+    public static final String REQUISITION_RECORD_5_REQUESTER_NAME = "RequesterName";
+    public static final String REQUISITION_RECORD_6_APPROVER_ID = "ApprovingStaffID";
+    public static final String REQUISITION_RECORD_7_APPROVER_NAME = "ApprovingStaffName";
+    public static final String REQUISITION_RECORD_8_APPROVED_DATE = "ApproveDate";
+    public static final String REQUISITION_RECORD_9_STATUS = "Status";
+    public static final String REQUISITION_RECORD_10_REQUEST_DATE = "RequestDate";
+    
+    
+    // FOR REQUISITION DETAIL
+    public static final String REQUISITION_DETAIL_1_REQUISITION_NO = "RequisitionNo";
+    public static final String REQUISITION_DETAIL_2_ITEM_CODE = "ItemCode";
+    public static final String REQUISITION_DETAIL_3_ITEM_DESCRIPTION = "StationeryDescription";
+    public static final String REQUISITION_DETAIL_4_ITEM_UOM = "UOM";
+    public static final String REQUISITION_DETAIL_5_REMARKS = "Remarks";
+    public static final String REQUISITION_DETAIL_6_REQUEST_QTY = "RequestQty";
+    public static final String REQUISITION_DETAIL_7_FULFILLED_QTY = "FulfilledQty";
+    public static final String REQUISITION_DETAIL_8_CLERK_ID = "ClerkID";
+    public static final String REQUISITION_DETAIL_9_RETRIEVED_DATE = "RetrievedDate";
+    public static final String REQUISITION_DETAIL_10_ALLOCATE_QTY = "AllocateQty";
+    public static final String REQUISITION_DETAIL_11_NEXT_COLLECTION_DATE = "NextCollectionDate";
+}
