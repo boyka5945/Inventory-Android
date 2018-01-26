212dc042ee3cbe6a8ee4944c8549d0403dfe2a98
czolbe88 <czolbe88@gmail.com>
2018-01-26 09:39:52 +0800

    added url for retrieve

--- app/src/main/java/com/example/yello/inventory_mvc/utility/UrlString.java
+++ app/src/main/java/com/example/yello/inventory_mvc/utility/UrlString.java
@@ -45,5 +45,11 @@ public final class UrlString
     // UriTemplate = "/GetAllRequisitionRecords"
     public static final String getAllRequisitionRecords = host + "/GetAllRequisitionRecords";
 
+    // UriTemplate = "/GetRetrievalList"
+    public static final String GetRetrievalList = host + "/GetRetrievalList";
+
+    // UriTemplate = "/UpdateRetrieval"
+    public static final String UpdateRetrieval = host + "/UpdateRetrieval";
+
 
 }
