a597085784e7a6b802e369cf777a3625903c1113
czolbe88 <czolbe88@gmail.com>
2018-01-26 17:24:30 +0800

    updated retrieve activity added allocation activity

---
+++ app/src/main/java/com/example/yello/inventory_mvc/activity/AllocationListActivity.java
@@ -0,0 +1,15 @@
+package com.example.yello.inventory_mvc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yello.inventory_mvc.R;
+
+public class AllocationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allocation_list);
+    }
+}
