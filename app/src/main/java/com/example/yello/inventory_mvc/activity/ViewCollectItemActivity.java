cc256d47b0e1b0fb8fb2af89f1b3f071d7de6996
WesLeeWH <33190336+WesLeeWH@users.noreply.github.com>
2018-01-27 10:18:39 +0800

    user representative collection use case

---
+++ app/src/main/java/com/example/yello/inventory_mvc/activity/ViewCollectItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.yello.inventory_mvc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.yello.inventory_mvc.R;
+
+public class ViewCollectItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_collect_item);
+    }
+}
