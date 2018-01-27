369afc6344fce8f4de8ab4097e10780d3ce97539
WesLeeWH <33190336+WesLeeWH@users.noreply.github.com>
2018-01-28 02:02:02 +0800

    user representative collection use case

--- app/src/main/java/com/example/yello/inventory_mvc/activity/ViewCollectItemActivity.java
+++ app/src/main/java/com/example/yello/inventory_mvc/activity/ViewCollectItemActivity.java
@@ -1,14 +1,56 @@
 package com.example.yello.inventory_mvc.activity;
 
+import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
 import com.example.yello.inventory_mvc.R;
+import com.example.yello.inventory_mvc.adapter.CollectItemAdapter;
+import com.example.yello.inventory_mvc.adapter.NewRequisitionFormAdapter;
+import com.example.yello.inventory_mvc.model.Disbursement;
+import com.example.yello.inventory_mvc.model.RequisitionForm;
 
-public class ViewCollectItemActivity extends AppCompatActivity {
+import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_collect_item);
-    }
-}
+//public class ViewCollectItemActivity extends AppCompatActivity {
+//
+//
+//    private ListView listView;
+//   private String url = "";
+//    private CollectItemAdapter adapter;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        listView = (ListView) this.findViewById(R.id.listView);
+//        adapter = new CollectItemAdapter(this, R.layout.collect_item_row,
+//                Disbursement.GetDisbursementList(url));
+//        listView.setAdapter(adapter);
+//
+//        registerForContextMenu(listView);
+//
+//        new AsyncTask<String, Void, List<Disbursement>>() {
+//            @Override
+//            protected List<Disbursement> doInBackground(String... params) {
+//                return Disbursement.GetDisbursementList(params[0]);
+//            }
+//            @Override
+//            protected void onPostExecute(List<Disbursement> result) {
+//               CollectItemAdapter adapter = new CollectItemAdapter(getApplicationContext(),
+//                        result, android.R.layout.,
+//                        new String[]{"StationeryDescription", "NeedQty","ActualQty"},
+//                        new int[]{ android.R.id.text1, android.R.id.text2,android.R.id.});
+//                setListAdapter(adapter);
+//            }
+//        }.execute(url);
+//    }
+//
+//}
--- app/src/main/java/com/example/yello/inventory_mvc/adapter/CollectItemAdapter.java
+++ app/src/main/java/com/example/yello/inventory_mvc/adapter/CollectItemAdapter.java
@@ -5,56 +5,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
+
 import android.widget.TextView;
 
 import com.example.yello.inventory_mvc.R;
+import com.example.yello.inventory_mvc.model.Disbursement;
+
+
+import java.util.List;
 
 /**
  * Created by weihan on 27/1/2018.
  */
-/*
 
 public class CollectItemAdapter extends ArrayAdapter<Disbursement>{
 
     private final Context context;
-    private final String[] values;
 
-    public CollectItemAdapter(Context context, String[] values) {
-        super(context, R.layout.collect_item_row, values);
+    private List<Disbursement> items;
+
+    int resource;
+
+
+    public CollectItemAdapter(Context context, int resource, List<Disbursement> items) {
+        super(context, resource, items);
+        this.resource = resource;
         this.context = context;
-        this.values = values;
+        this.items = items;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View v = inflater.inflate(resource, null);
+        final Disbursement disburse = items.get(position);
+     View rowView = inflater.inflate(R.layout.collect_item_row, parent, false);
+        TextView textViewItemName = (TextView) rowView.findViewById(R.id.textView5);
+        TextView textViewItemCat = (TextView) rowView.findViewById(R.id.textView6);
+        TextView textViewItemQty = (TextView) rowView.findViewById(R.id.textView7);
 
-        View rowView = inflater.inflate(R.layout.collect_item_row, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.textView);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
-        textView.setText(values[position]);
+//        textViewItemName.setText(items[position]);
+//
+//        // Change icon based on name
+//        String s = items[position];
 
-        // Change icon based on name
-        String s = values[position];
+        System.out.println(disburse);
 
-        System.out.println(s);
-
-        if (s.equals("WindowsMobile")) {
-            imageView.setImageResource(R.drawable.windowsmobile_logo);
-        } else if (s.equals("iOS")) {
-            imageView.setImageResource(R.drawable.ios_logo);
-        } else if (s.equals("Blackberry")) {
-            imageView.setImageResource(R.drawable.blackberry_logo);
-        } else {
-            imageView.setImageResource(R.drawable.android_logo);
+        if (items != null) {
+            ((TextView) rowView.findViewById(R.id.textView5)).setText(
+                    disburse.get("StationeryDescription"));
+            ((TextView) rowView.findViewById(R.id.textView6)).setText(disburse.get(
+                    "ActualQty"));
+            ((TextView) rowView.findViewById(R.id.textView7)).setText(disburse.get(
+                    "NeedQty"));
         }
-
         return rowView;
     }
-*/
 
-//
-//}
+
+}
